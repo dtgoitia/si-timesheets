b780256cfcdf9192839d0a114360fdc99ef86c98
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-03-28 09:57:02 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-cp ./dist/timesheet.exe ./timesheet.exe
 git checkout master
 git checkout dev ./timesheet.exe
-git checkout add ./timesheet.exe
+git add timesheet.exe
 git commit -m "Deploy"
 git push origin master
 git checkout dev
