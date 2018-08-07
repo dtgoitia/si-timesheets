c57e3f014d639cd3862c9332cf2350fa9a0b3df9
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-08-07 09:02:39 +0100

    feat: Implement 'click' library

--- sit/__main__.py
+++ sit/__main__.py
@@ -1,6 +1,4 @@
-from sit.load import get_config
-from sit.spreadsheet import update_spreadsheet
-from sit.mail import send_mail
+from sit.cli import sit
 
 
 # TODO: move global variables to default argument values for click commands
@@ -8,23 +6,8 @@ CONFIG_PATH = "./.config"
 
 
 def main():
-    """Run main function (on 'sit' command)."""
-    config = get_config(CONFIG_PATH)
-    template_file_path = config["xlsx_template_path"]
-    employee_name = config["employee_name"]
-    new_spreadsheet_path = update_spreadsheet(template_file_path, employee_name)
-    if new_spreadsheet_path is not None:
-        print(f"new_spreadsheet_path = {new_spreadsheet_path}")
-        send_mail(config['microsoft_account_email'],
-                  config['microsoft_account_pass'],
-                  config['email_subject'],
-                  config['email_body'],
-                  config['email_to'],
-                  new_spreadsheet_path,
-                  config['delete_temp'])
-    else:
-        print('new_spreadsheet_is_created === false')
-    print('main() executed till the end!')
+    """Run 'sit' CLI command."""
+    sit()
 
 
 # Uncomment to debug
