61d76ec86bd666a1bec372c2e54db4aa39d78bd6
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-09-26 14:11:03 +0100

    chore: Automate package extraction from Pipfile

--- setup.py
+++ setup.py
@@ -3,6 +3,26 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from setuptools import find_packages, setup
 
+
+def get_packages_from_pipfile():
+    """Return package names from Pipfile."""
+    with open('Pipfile', 'r') as f:
+        content = f.read()
+    packages = []
+    extract_line = False
+    for line in content.split('\n'):
+        if line == '[packages]':
+            extract_line = True
+            continue
+        if line == '':
+            extract_line = False
+            continue
+        if extract_line:
+            package_name = line.split(' ')[0]
+            packages.append(package_name)
+    return packages
+
+
 setup(
     name='sit',
     version='0.0.1',
@@ -16,7 +36,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     keywords=['timesheets'],
-    install_requires=['click', 'openpyxl'],
+    install_requires=get_packages_from_pipfile(),
     entry_points={
         'console_scripts': [
             'sit=sit.__main__:main'
