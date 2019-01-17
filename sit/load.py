56cafafed0d043a6b3760c1fa6742395c5de588a
David Torralba Goitia <david.torralba.goitia@gmail.com>
2019-01-17 10:49:19 +0000

    feat: Move the configuration file to ~/.config/

--- sit/load.py
+++ sit/load.py
@@ -1,20 +1,16 @@
 import json
-import errno
 import os
-from pathlib import Path
 
 
-DEFAULT_FILE_PATH = str(Path(Path.home(), '.sit/.config'))
+DEFAULT_FILE_PATH = os.path.join(os.path.expanduser('~'), '.config', 'sit', '.config')
 
 
 def get_config(file_path=DEFAULT_FILE_PATH) -> dict:
     """Import JSON configuration.
 
     If no file path is provided, try to import from the default
-    path `~/.sit/.config`
+    path `~/.config/sit/.config`
     """
-    if not Path(file_path).exists():
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file_path)
     with open(file_path) as fd:
         config = json.load(fd)
         return config
